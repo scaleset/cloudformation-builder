45c6483ce6a817d5772df1bd48e660413e863eb7
Marvin Munoz Baron <mmunozba@outlook.com>
2018-01-06 19:51:57 +0100

    Add source property and update comments

--- src/main/java/com/scaleset/cfbuilder/ec2/metadata/CFNFile.java
+++ src/main/java/com/scaleset/cfbuilder/ec2/metadata/CFNFile.java
@@ -5,13 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 /**
- * ATM only supports Strings as content, no source url
+ * Class representing Files in the AWS::Cloudformation::Init
  */
-@JsonPropertyOrder({"content", "encoding", "mode", "owner", "group"})
+@JsonPropertyOrder({"content", "source", "encoding", "mode", "owner", "group"})
 public class CFNFile {
 
+    // Note: Content and source are mutually exclusive.
     @JsonProperty("content")
     public String content;
+
+    @JsonProperty ("source")
+    public String source;
 
     @JsonProperty("encoding")
     public String encoding;
@@ -25,22 +29,34 @@ public class CFNFile {
     @JsonProperty("group")
     public String group;
 
-    //TODO add: source(source and content exclude each other)
-
+    // The id represents the path where the file will be stored
     @JsonIgnore
     public String id;
 
     /**
-     * The id is the path where the file will be stored
+     * Creates a <tt>CFNFile<tt> with the given id.
      *
-     * @param id
+     * @param id for the <tt>CFNFile<tt> to be created
      */
     public CFNFile(String id) {
         this.id = id;
     }
 
     public CFNFile setContent(String content) {
+        // Set source to null if it exists
+        if (!this.source.equals(null)){
+            this.source = null;
+        }
         this.content = content;
+        return this;
+    }
+
+    public CFNFile setSource(String source) {
+        // Set content to null if it exists
+        if (!this.content.equals(null)){
+            this.content = null;
+        }
+        this.source = source;
         return this;
     }
 
